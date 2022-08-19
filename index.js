a6a981918ce9ba808981aea2407df87556c0f451
Alok-112 <105195422+Alok-112@users.noreply.github.com>
2022-08-19 11:44:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let btnEl = document.querySelector(".btn")
 
 btnEl.addEventListener("mouseover", (event) => {
-    let x = event.pageX - btnEl.offsetTop;
-    let y = event.pageX - btnEl.offsetTop;
+    let x = (event.pageX - btnEl.offsetLeft);
+    let y = (event.pageY - btnEl.offsetTop);
 
 
     btnEl.style.setProperty("--xPos", x + "px");
